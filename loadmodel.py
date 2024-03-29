ab3c0d7277fa509b58e7f53f6d5ecab249dadf8e
Mustafa250IITJ <m22aie250@iitj.ac.in>
2024-03-29 23:11:17 +0530

    modified the Activity3 version1 and save resnet50 model in version2.0

--- loadmodel.py
+++ loadmodel.py
@@ -4,3 +4,6 @@ import torchvision.models as models
 # download ResNet model
 # resnet101 = models.resnet101(pretrained=True)
 resnet50 = models.resnet50(pretrained=True)
+
+# save the model to a folder
+torch.save(resnet50.state_dict(), 'resnet50.pth')
